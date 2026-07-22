42c316ee2e109f747b6b27ad6beda5b9074fbce8
Sashagirl1986 <sassha.1986.23@gmail.com>
2026-07-22 22:11:17 +0300

    Update app.js

--- css/js/app.js
+++ css/js/app.js
@@ -1,1 +1,42 @@
+let selectedTest = "light";
 
+const cards = document.querySelectorAll(".test-card");
+
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        cards.forEach(c => c.classList.remove("selected"));
+
+        card.classList.add("selected");
+
+        selectedTest = card.id;
+
+    });
+
+});
+
+document.getElementById("startBtn").addEventListener("click", () => {
+
+    const fullname = document.getElementById("fullname").value.trim();
+
+    if(fullname === ""){
+
+        alert("Введіть своє ПІБ");
+
+        return;
+
+    }
+
+    alert(
+`Вітаємо!
+
+ПІБ: ${fullname}
+
+Обраний тест:
+${selectedTest.toUpperCase()}
+
+У наступній версії тут відкриється тест.`
+    );
+
+});
